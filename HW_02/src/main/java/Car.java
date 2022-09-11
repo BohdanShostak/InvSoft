bf0a42dac2b5ba2d06d980305b9c04bc579102c6
Bohdan Shostak <bohdanshostak93@gmail.com>
2022-09-11 15:27:04 +0300

    Created basic structure of the project

---
+++ HW_02/src/main/java/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    private String brand;
+    private String model;
+    private int maxSpeed;
+    private double fuelConsumption;
+
+    public Car(String brand, String model, int maxSpeed, double fuelConsumption) {
+        this.brand = brand;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+}
